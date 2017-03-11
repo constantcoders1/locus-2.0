b1b0c1a23a1811f4e21f1cf6809d73025334d0f7
Kathleen Catlett <kathleencatlett@comcast.net>
2017-03-11 15:01:26 -0500

    added loginteacher loginstudent

--- public/js/loginteacher.js
+++ public/js/loginteacher.js
@@ -3,10 +3,8 @@ $(document).ready(function() {
   var loginForm = $("form.login");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
-  var tableCheck = $("input#studentorteacher")
-
-
-  console.log(tableCheck)
+ 
+ console.log("teacher login")
 
   // When the form is submitted, we validate there's an email and password entered
 
@@ -19,7 +17,6 @@ $(document).ready(function() {
     var userData = {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim(),
-      table: tableCheck
     };
 
     if (!userData.email || !userData.password) {
@@ -27,11 +24,9 @@ $(document).ready(function() {
     }
 
     // If we have an email and password we run the loginUser function and clear the form
-    if(tableCheck == "student"){
-      loginStudent(userData.email, userData.password);
-    } else {
-      loginTeacher(userData.email, userData.password);
-    }
+  
+    loginTeacher(userData.email, userData.password);
+    
     emailInput.val("");
     passwordInput.val("");
   
@@ -41,19 +36,6 @@ $(document).ready(function() {
  
  function loginTeacher(email, password) {
     $.post("/api/login/teacher", {
-      email: email,
-      password: password,
-    }).then(function(data) {
-      window.location.replace(data);
-      // If there's an error, log the error
-    }).catch(function(err) {
-      console.log(err);
-    });
-  }
-
-
- function loginStudent(email, password) {
-    $.post("/api/login/student", {
       email: email,
       password: password,
     }).then(function(data) {
