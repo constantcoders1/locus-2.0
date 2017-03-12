5bf481ee7eff0d8048fd56a6c50a5be6cf7e9983
smchow <sunita.m.chowdhury@gmail.com>
2017-03-11 21:27:23 -0500

    render field notes page

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -2,6 +2,12 @@ var bcrypt = require('bcryptjs');
 var express = require('express');
 var router  = express.Router();
 var mysql = require('mysql')
+// Requiring path to so we can use relative routes to our HTML files
+var path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 
 var connection = mysql.createConnection({
   host: "localhost",
