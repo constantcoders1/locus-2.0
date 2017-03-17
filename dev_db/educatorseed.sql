fc7f45c76bbcb15f05c68bc08f2035158f86b656
Kathleen Catlett <kathleencatlett@comcast.net>
2017-03-17 16:17:46 -0400

    correct format for seeds

--- dev_db/educatorseed.sql
+++ dev_db/educatorseed.sql
@@ -1,4 +1,9 @@
-id,email,password,username,createdAt,updatedAt
-1,phil@princeton.edu,$2a$10$6dXauFqPyY2/dQEN3ubuP.9hGfwoPaaLqtJ35DNi07TWjtyR1i9EO,ProfPhilander,"2017-03-17 19:53:58","2017-03-17 19:53:58"
-2,bill@gmail.com,$2a$10$LvM6echHhi1wqCjngrFQRekGcLXhQtELjSSMgz1G.j1DH4hgVIzDi,BillNye,"2017-03-17 19:54:25","2017-03-17 19:54:25"
-3,Neil@gmail.com,$2a$10$yQJF94/ObF9HKjoxHcT/Out0UVmUrH6K/EG06Vb56mHq03Y0cRzxi,NeilDT,"2017-03-17 19:54:52","2017-03-17 19:54:52"
+/*
+-- Query: SELECT * FROM observations_db.educators
+LIMIT 0, 1000
+
+-- Date: 2017-03-17 16:16
+*/
+INSERT INTO `educators` (`id`,`email`,`password`,`username`,`createdAt`,`updatedAt`) VALUES (1,'phil@princeton.edu','$2a$10$6dXauFqPyY2/dQEN3ubuP.9hGfwoPaaLqtJ35DNi07TWjtyR1i9EO','ProfPhilander','2017-03-17 19:53:58','2017-03-17 19:53:58');
+INSERT INTO `educators` (`id`,`email`,`password`,`username`,`createdAt`,`updatedAt`) VALUES (2,'bill@gmail.com','$2a$10$LvM6echHhi1wqCjngrFQRekGcLXhQtELjSSMgz1G.j1DH4hgVIzDi','BillNye','2017-03-17 19:54:25','2017-03-17 19:54:25');
+INSERT INTO `educators` (`id`,`email`,`password`,`username`,`createdAt`,`updatedAt`) VALUES (3,'Neil@gmail.com','$2a$10$yQJF94/ObF9HKjoxHcT/Out0UVmUrH6K/EG06Vb56mHq03Y0cRzxi','NeilDT','2017-03-17 19:54:52','2017-03-17 19:54:52');
