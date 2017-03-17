9a4fcfc462762c82f3071d105c24bd478d320dae
Kathleen Catlett <kathleencatlett@comcast.net>
2017-03-17 12:27:27 -0400

    add can get teacher but not project ??

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -39,7 +39,7 @@ module.exports = function(app) {
   
 
   app.get("/api/teachers", function(req, res) {
-    db.Teacher.findAll({
+    db.Educator.findAll({
      
     }).then(function(dbTeacher) {
       res.json(dbTeacher);
