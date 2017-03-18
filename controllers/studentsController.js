794f4ebb453671c067885500e6fba6859a9086f0
Kristen Manning <kristenm@Kristens-MacBook-Pro.local>
2017-03-18 00:08:54 -0400

    students' join projects button appears only on projects they're not already signed up for

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -90,50 +90,59 @@ router.get('/my-data/:studentid', function(req,res){
     });
 });
 
+router.get('/viewprojects', isAuthenticated, function(req, res) {
+  // get all projects from database 
+  if (req.user){
+    db.Project.findAll({include: [db.Educator]
+    }).then(function(dbProject1) {
+        // Find projects student is already signed up for 
+        db.StudentToProject.findAll({where: {StudentId: req.user.id}}).then(function(dbS2P) {
+          var matches = []
+          for (i in dbS2P){
+            matches.push(dbS2P[i].dataValues.ProjectId)
+          }
+
+          var data = []
+          for (p in dbProject1){
+            if (matches.indexOf(dbProject1[p].dataValues.id) > -1){
+              dbProject1[p]["match"] = true
+            }else{
+              dbProject1[p]["match"] = false
+            }
+            data.push(dbProject1[p])
+
+          }
+          console.log(data)
+     //      var objForHandlebars = {"data": dbProject1, "Matches": dbS2P}
+     //      console.log(objForHandlebars["Matches"])
+
+     // console.log(objForHandlebars)
+    
+    res.render("students/project-view", {"data": data})
+    });
+  });
+  }
+});
 
 // Form for posting data 
 // Get project(s) this student is working to display as options in the form 
 
-router.get('/new-entry/:studentid/:projectid', function(req,res){
-	// db.Fieldnotes.findAll({}).then(function(dbFieldnotes) {
-      res.send("Student's form for posting new data ");;
-    // });
-});
+// router.get('/new-entry/:studentid/:projectid', function(req,res){
+// 	// db.Fieldnotes.findAll({}).then(function(dbFieldnotes) {
+//       res.send("Student's form for posting new data ");;
+//     // });
+// });
 
 // Post new entry to the database 
 
-router.post('/join-project/:studentid/', function(req,res){
-	// db.Fieldnotes.findAll({}).then(function(dbFieldnotes) {
-      res.send("Student posted new entry to the database");;
-    // });
+router.post('/join-project/:projectid', isAuthenticated, function(req,res){
+  db.StudentToProject.create({
+          ProjectId: req.params.projectid,
+          StudentId: req.user.id,
+      }).then(function(dbFieldnotes) {
+  res.redirect("/notes/view/"+req.params.projectid);;
+    });
 });
 
-// TBI: Edit an existing entry 
-
-// router.put('/', function(req,res){
-// 	db.Fieldnotes.update({
-// 		// Form
-// 	      // text: req.body.text,
-// 	      // complete: req.body.complete
-//     }, {
-//       where: {
-//         id: req.body.id
-//       }
-//     }).then(function(dbFieldnotes) {
-//       res.send("Student Entry Edit");;
-//     });
-// });
-
-// TBI: Delete an existing entry 
-
-// router.delete('/', function(req,res){
-//     db.Fieldnotes.destroy({
-//       where: {
-//         id: req.params.id
-//       }
-//     }).then(function(dbFieldnotes) {
-//       res.send("Student Entry Delete");;
-//     });
-// });
 
 module.exports = router;
