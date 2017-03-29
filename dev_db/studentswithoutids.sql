d8c0301c5e8d836552606ae012813bf23e9520ca
Kathleen Catlett <kathleencatlett@comcast.net>
2017-03-29 09:29:38 -0400

    added seed files from db on my pc

---
+++ dev_db/studentswithoutids.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM observations_db.students
+LIMIT 0, 1000
+
+-- Date: 2017-03-28 18:29
+*/
+INSERT INTO `students` (`email`,`password`,`username`,`country`,`state`,`city`,`createdAt`,`updatedAt`) VALUES ('joe@joe.joe','$2a$10$Op3wgtXHRA6XDsPaTIHWhOQZrNzeODeFAwCJ03qDtbrOMaUnYR8gq','joe','United States of America','NY','kljklj','2017-03-18 02:00:28','2017-03-18 02:00:28');
+INSERT INTO `students` (`email`,`password`,`username`,`country`,`state`,`city`,`createdAt`,`updatedAt`) VALUES ('ann@ann.ann','$2a$10$p4RCoX.kSyeZiT0XiCQPNeTqoflTd4GMcIH1vEEqm9VxEh881Xxwu','ann','United States of America','NM','kljklj','2017-03-18 02:02:39','2017-03-18 02:02:39');
+INSERT INTO `students` (`email`,`password`,`username`,`country`,`state`,`city`,`createdAt`,`updatedAt`) VALUES ('mary@mary.mary','$2a$10$wOFbD/LuUU5wf20jl9BdCeatEbDp.er1AIIVJo9/rhm8LdJQ4c9TW','mary','South Africa','','kljklj','2017-03-18 02:03:13','2017-03-18 02:03:13');
+INSERT INTO `students` (`email`,`password`,`username`,`country`,`state`,`city`,`createdAt`,`updatedAt`) VALUES ('sue@sue.sue','$2a$10$lHjzhAJS9UGzQKRXUL/LLuJsQVUj7vvxCVEeDoW0Ragu5.oBbBpou','sue','South Africa','','kljklj','2017-03-18 02:03:39','2017-03-18 02:03:39');
+INSERT INTO `students` (`email`,`password`,`username`,`country`,`state`,`city`,`createdAt`,`updatedAt`) VALUES ('tom@tom.tom','$2a$10$7J551lTF0FpBVvALffHAf.igqWgiFbjTitZ21Nh4fpEeGLJpX9xii','tom','United States of America','NJ','kljklj','2017-03-18 02:06:34','2017-03-18 02:06:34');
+INSERT INTO `students` (`email`,`password`,`username`,`country`,`state`,`city`,`createdAt`,`updatedAt`) VALUES ('ed@ed.ed','$2a$10$Z50CNhPvE1QiARmXxmKi0.hLmcAsN1KS6nF5MquRDEsEIg3Yth6gu','ed','United States of America','NY','kljklj','2017-03-18 02:06:59','2017-03-18 02:06:59');
+INSERT INTO `students` (`email`,`password`,`username`,`country`,`state`,`city`,`createdAt`,`updatedAt`) VALUES ('amy@amy.amy','$2a$10$tRvtDTeDTxoBTHTTOdi4LO1Iv..LsADTQbrRyfG0z1SH4Fcgzp.Oy','amy','United States of America','NJ','kljklj','2017-03-18 02:07:51','2017-03-18 02:07:51');
+INSERT INTO `students` (`email`,`password`,`username`,`country`,`state`,`city`,`createdAt`,`updatedAt`) VALUES ('vin@vin.vin','$2a$10$sPiImCa5wV5/mZBMiPy3e.D9.CPiW.z.qiXV9COyUyEKRCzJ0QgdG','vin','United States of America','NJ','kljklj','2017-03-18 02:09:28','2017-03-18 02:09:28');
+INSERT INTO `students` (`email`,`password`,`username`,`country`,`state`,`city`,`createdAt`,`updatedAt`) VALUES ('sam@sam.sam','$2a$10$oN5aok3PL7wvYBbGEOKzU.y8TQPL5xrRE09L.gNoh3109TBJb6ELq','sam','South Africa','','kljklj','2017-03-18 02:10:25','2017-03-18 02:10:25');
+INSERT INTO `students` (`email`,`password`,`username`,`country`,`state`,`city`,`createdAt`,`updatedAt`) VALUES ('val@val.val','$2a$10$wz5TC.cXHWrcKuj7cYdCgeZOKJckV76bQ/P.ZY.zAyaFSl.qPx5mm','val','United States of America','NM','kljklj','2017-03-18 02:11:28','2017-03-18 02:11:28');
