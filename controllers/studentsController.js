fde3a2b6b8f9c190d67172aace5c5ea7ef1b2a18
smchow <sunita.m.chowdhury@gmail.com>
2017-06-06 12:53:47 -0400

    Fixed json

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -252,10 +252,10 @@ router.get('/leaderboard', isAuthenticated, function(req, res) {
 }).then(function (data) {
   res.json(data)
 });*/
-db.sequelize.query("select count(*) projCt, st.username un, pj.name  pi , YEAR(fn.UpdatedAt) yr, MONTH(fn.UpdatedAt) mo , MONTHNAME(fn.UpdatedAt) mname from fieldnotes fn, students st, projects pj where fn.StudentId = st.id and pj.id = fn.ProjectId group by un, pi, yr, mo, mname order by projCt desc"
+db.sequelize.query("select count(*) projCt, st.username uname, pj.name  pname , YEAR(fn.UpdatedAt) yr, MONTH(fn.UpdatedAt) mo , MONTHNAME(fn.UpdatedAt) mname from fieldnotes fn, students st, projects pj where fn.StudentId = st.id and pj.id = fn.ProjectId group by uname, pname, yr, mo, mname having projCt > 2 order by projCt desc"
   ).then(function(data) {
-    res.json(JSON.stringify(data));
-});
+    res.json(data[0]);
+}); //JSON.stringify(data)
   /*if (req.user){
     db.Project.findAll({include: [db.Educator]
     }).then(function(dbProject1) {
