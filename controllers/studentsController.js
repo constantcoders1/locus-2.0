3206baf004064dd174027bc6f679bde5fa18e824
Kathleen Catlett <kathleencatlett@comcast.net>
2017-06-21 20:18:10 -0400

    another typo fixed

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -227,7 +227,7 @@ router.get('/leaderboard', isAuthenticated, function(req, res) {
   res.json(data)
 });*/
 
-db.sequelize.query("select count(*) project, st.username uname, pj.name  pname , YEAR(fn.UpdatedAt) yr, MONTH(fn.UpdatedAt) mo , MONTHNAME(fn.UpdatedAt) mname from fieldnotes fn, students st, projects pj where fn.StudentId = st.id and pj.id = fn.ProjectId group by uname, pname, yr, mo, mname having project > 2 order by  yr desc, mo desc, project desc"
+db.sequelize.query("select count(*) project, st.username uname, pj.name  pname , YEAR(fn.UpdatedAt) yr, MONTH(fn.UpdatedAt) mo , MONTHNAME(fn.UpdatedAt) mname from Fieldnotes fn, students st, projects pj where fn.StudentId = st.id and pj.id = fn.ProjectId group by uname, pname, yr, mo, mname having project > 2 order by  yr desc, mo desc, project desc"
 
   ).then(function(data) {
     //res.json(data[0]);
